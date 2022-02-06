70b14351186820a3d059f6bb8e9a6150d8202b89
delete44 <delete44+tech@protonmail.com>
2022-02-06 14:20:47 +0000

    Updated tailwind config to extend existing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      orange: "#cc6f33",
+    extend: {
+      colors: {
+        orange: "#cc6f33",
+      },
+      fontFamily: {
+        title: ["Heavyweight"],
+      },
     },
-    fontFamily: {
-      title: ["Heavyweight"],
-    },
-    extend: {},
   },
   plugins: [],
 };
