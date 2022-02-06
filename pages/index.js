3531d081fdf29b301cd88943310132f93f6a7e61
delete44 <delete44+tech@protonmail.com>
2022-02-06 13:47:03 +0000

    First use of tailwind

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-3xl font-bold underline">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
