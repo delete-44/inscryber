d58d6bfb0f1317f9740befae798a78f51418d1d2
delete44 <delete44+tech@protonmail.com>
2022-02-06 14:43:52 +0000

    Added base grid layout. Added demo card

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,9 +11,26 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="text-7xl text-orange font-title text-shadow-orange">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="text-7xl text-orange font-title text-shadow-orange text-center my-5">
+          Inscryber
         </h1>
+
+        <div class="md:grid md:grid-cols-3 md:gap-4 w-100 md:w-4/5 mx-auto text-center">
+          <section class="col-span-2">
+            <h2 className="text-5xl text-orange font-title text-shadow-orange">
+              Welcome, new Scrybe...
+            </h2>
+          </section>
+
+          <section>
+            <Image
+              src="https://res.cloudinary.com/delete-44/image/upload/c_scale,l_v1644060029:Inscryption:rough_stinky_ijycvc.svg,w_248,y_340/v1644060066/Inscryption/blank_card_mvzub7.webp"
+              alt="A blank card with the 'Stinky' sigil"
+              width={640}
+              height={1048}
+            />
+          </section>
+        </div>
       </main>
 
       <footer>&copy; delete44</footer>
