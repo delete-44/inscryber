eb5267c95be305934c08e955c597cb679e83a494
delete44 <delete44+tech@protonmail.com>
2022-02-08 20:41:15 +0000

    Added tests for remaining description

--- __tests__/name.test.js
+++ __tests__/name.test.js
@@ -1,15 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Name from "../components/name";
 
-describe('Name', () => {
-  it('renders a name label', () => {
-    render(<Name setNameTF={() => {}}/>)
+describe("Name", () => {
+  beforeEach(() => {
+    render(<Name setNameTF={() => {}} />);
+  });
 
-    const nameField = screen.getByRole('textbox', {
+  it("renders a name text box", () => {
+    const nameField = screen.getByRole("textbox", {
       name: /Name/,
-    })
+    });
 
     expect(nameField).toBeInTheDocument();
-  })
-})
+    expect(nameField).toHaveTextContent("");
+  });
+
+  it("renders a flavourful description", () => {
+    const nameFlavour = screen.getByText("Tell me this creature's .");
+    const nameLabel = screen.getByText("name");
+
+    expect(nameFlavour).toBeInTheDocument();
+    expect(nameLabel).toBeInTheDocument();
+  });
+});
