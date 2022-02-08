0245cedf908d7eafd6c18f34de5ddc3b7c5e62ce
delete44 <delete44+tech@protonmail.com>
2022-02-08 19:21:46 +0000

    Minor update to footer to show copyright info

--- pages/index.js
+++ pages/index.js
@@ -145,6 +145,12 @@ export default function Home() {
           </div>
         </div>
       </main>
+
+      <footer className="bg-white">
+        &copy; <a href="https://github.com/delete-44/">@delete44</a> [this
+        website] | &copy; <a href="https://github.com/vladdeSV">@vladdeSV</a>{" "}
+        [base card assets]
+      </footer>
     </div>
   );
 }
