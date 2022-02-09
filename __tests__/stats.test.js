251727561ee1764096c53eeab720b76c69e78a1e
delete44 <delete44+tech@protonmail.com>
2022-02-09 19:05:17 +0000

    Added basic behaviour tests

--- __tests__/stats.test.js
+++ __tests__/stats.test.js
@@ -18,8 +18,8 @@ describe("Stats", () => {
     });
 
     const healthField = screen.getByRole("spinbutton", {
-      name: /Health/
-    })
+      name: /Health/,
+    });
 
     expect(powerField).toBeInTheDocument();
     expect(powerField).toHaveTextContent("");
@@ -36,5 +36,35 @@ describe("Stats", () => {
     expect(flavour).toBeInTheDocument();
     expect(powerLabel).toBeInTheDocument();
     expect(healthLabel).toBeInTheDocument();
+  });
+
+  describe("the power number field", () => {
+    it("uses fit cropping for numbers < 10", async () => {
+      const powerField = screen.getByRole("spinbutton", {
+        name: /Power/,
+      });
+
+      userEvent.type(powerField, "9");
+
+      await waitFor(() => {
+        expect(mockCallback).toHaveBeenCalledWith(
+          `l_text:${HEAVYWEIGHT}_156:9,g_south_west,x_64,y_164,w_100,h_156,c_fit/`
+        );
+      });
+    });
+
+    it("uses scale cropping for numbers >= 10", async () => {
+      const powerField = screen.getByRole("spinbutton", {
+        name: /Power/,
+      });
+
+      userEvent.type(powerField, "10");
+
+      await waitFor(() => {
+        expect(mockCallback).toHaveBeenCalledWith(
+          `l_text:${HEAVYWEIGHT}_156:10,g_south_west,x_64,y_164,w_100,h_156,c_scale/`
+        );
+      });
+    });
   });
 });
