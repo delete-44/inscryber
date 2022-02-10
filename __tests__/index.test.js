1e795bdfac4af237b4d9854d8868dda5fe77c832
delete44 <delete44+tech@protonmail.com>
2022-02-10 17:19:04 +0000

    Added test to confirm name change does not fire request immediately

---
+++ __tests__/index.test.js
@@ -0,0 +1,55 @@
+import { act, render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import * as constants from "../components/constants";
+import Home from "../pages";
+
+describe("Name", () => {
+  constants.CLOUDINARY_BASE = "https://test/";
+
+  beforeEach(async () => {
+    render(<Home />);
+
+    jest.useFakeTimers();
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it("mocks the image url to avoid contacting cloudinary", async () => {
+    const image = await screen.findByAltText(
+      "A blank card with the 'Stinky' sigil"
+    );
+
+    expect(image.src).not.toContain("cloudinary");
+    expect(image.src).toContain("test");
+  });
+
+  it("staggers name changes to only fire request after user stops typing", async () => {
+    const image = await screen.findByAltText(
+      "A blank card with the 'Stinky' sigil"
+    );
+
+    expect(image.src).not.toContain("HEAVYWEIGHT.ttf_128");
+
+    const nameField = screen.getByRole("textbox", {
+      name: /Name/,
+    });
+
+    await act(async () => {
+      userEvent.type(nameField, "123456789");
+    });
+
+    jest.advanceTimersByTime(499);
+    expect(image.src).not.toContain("HEAVYWEIGHT.ttf_128");
+
+    await act(async () => {
+      jest.advanceTimersByTime(2);
+    });
+
+    expect(image.src).toContain("HEAVYWEIGHT.ttf_128");
+  });
+});
