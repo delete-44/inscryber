d1c718ff6adc60ae32344829f3755717779df881
delete44 <delete44+tech@protonmail.com>
2022-02-11 23:08:09 +0000

    Iterated over collection to clean up component

--- components/constants.js
+++ components/constants.js
@@ -1,3 +1,36 @@
 export const HEAVYWEIGHT = "v1644177732:Inscryption:HEAVYWEIGHT.ttf";
 export const CLOUDINARY_BASE = "https://res.cloudinary.com/delete-44/image/upload/";
 export const CARD_BASE = "v1644060066/Inscryption/blank_vladde"
+
+export const SIGILS = [
+  { id: "v1644605839", filename: "airborne", label: "Airborne" },
+  { id: "v1644619391", filename: "ant_spawner", label: "Ant Spawner" },
+  { id: "v1644619391", filename: "bees_within", label: "Bees Within" },
+  { id: "v1644619391", filename: "bellist", label: "Bellist" },
+  { id: "v1644605839", filename: "bifurcated_strike", label: "Bifurcated Strike" },
+  { id: "v1644619391", filename: "bone_king", label: "Bone King" },
+  { id: "v1644619391", filename: "burrower", label: "Burrower" },
+  { id: "v1644619389", filename: "corpse_eater", label: "Corpse Eater" },
+  { id: "v1644619389", filename: "dam_builder", label: "Dam Builder" },
+  { id: "v1644619389", filename: "fecundity", label: "Fecundity" },
+  { id: "v1644619389", filename: "fledgeling", label: "Fledgeling" },
+  { id: "v1644619389", filename: "frozen_away", label: "Frozen Away" },
+  { id: "v1644619389", filename: "guardian", label: "Guardian" },
+  { id: "v1644619390", filename: "hefty", label: "Hefty" },
+  { id: "v1644619390", filename: "hoarder", label: "Hoarder" },
+  { id: "v1644619390", filename: "leader", label: "Leader" },
+  { id: "v1644619390", filename: "loose_tail", label: "Loose Tail" },
+  { id: "v1644619390", filename: "many_lives", label: "Many Lives" },
+  { id: "v1644615059", filename: "mighty_leap", label: "Mighty Leap" },
+  { id: "v1644619390", filename: "rabbithole", label: "Rabbithole" },
+  { id: "v1644619390", filename: "random", label: "Random" },
+  { id: "v1644619390", filename: "sharp_quills", label: "Sharp Quills" },
+  { id: "v1644619390", filename: "sprinter", label: "Sprinter" },
+  { id: "v1644619390", filename: "stinky", label: "Stinky" },
+  { id: "v1644619390", filename: "touch_of_death", label: "Touch of Death" },
+  { id: "v1644613749", filename: "trifurcated_strike", label: "Trifurcated Strike" },
+  { id: "v1644619390", filename: "trinket_bearer", label: "Trinket Bearer" },
+  { id: "v1644619390", filename: "unkillable", label: "Unkillable" },
+  { id: "v1644619391", filename: "waterborne", label: "Waterborne" },
+  { id: "v1644619391", filename: "worthy_sacrifice", label: "Worthy Sacrifice" },
+]
--- components/sigils.js
+++ components/sigils.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Select from "react-select";
+import { SIGILS } from "./constants";
 
 const Sigils = (props) => {
   const [sigil, setSigil] = useState("");
@@ -7,36 +8,12 @@ const Sigils = (props) => {
 
   const options = [
     { value: "", label: "No sigils" },
-    { value: "v1644605839:Inscryption:Sigils:airborne", label: "Airborne" },
-    { value: "v1644619391:Inscryption:Sigils:ant_spawner", label: "Ant Spawner" },
-    { value: "v1644619391:Inscryption:Sigils:bees_within", label: "Bees Within" },
-    { value: "v1644619391:Inscryption:Sigils:bellist", label: "Bellist" },
-    { value: "v1644605839:Inscryption:Sigils:bifurcated_strike", label: "Bifurcated Strike" },
-    { value: "v1644619391:Inscryption:Sigils:bone_king", label: "Bone King" },
-    { value: "v1644619391:Inscryption:Sigils:burrower", label: "Burrower" },
-    { value: "v1644619389:Inscryption:Sigils:corpse_eater", label: "Corpse Eater" },
-    { value: "v1644619389:Inscryption:Sigils:dam_builder", label: "Dam Builder" },
-    { value: "v1644619389:Inscryption:Sigils:fecundity", label: "Fecundity" },
-    { value: "v1644619389:Inscryption:Sigils:fledgeling", label: "Fledgeling" },
-    { value: "v1644619389:Inscryption:Sigils:frozen_away", label: "Frozen Away" },
-    { value: "v1644619389:Inscryption:Sigils:guardian", label: "Guardian" },
-    { value: "v1644619390:Inscryption:Sigils:hefty", label: "Hefty" },
-    { value: "v1644619390:Inscryption:Sigils:hoarder", label: "Hoarder" },
-    { value: "v1644619390:Inscryption:Sigils:leader", label: "Leader" },
-    { value: "v1644619390:Inscryption:Sigils:loose_tail", label: "Loose Tail" },
-    { value: "v1644619390:Inscryption:Sigils:many_lives", label: "Many Lives" },
-    { value: "v1644615059:Inscryption:Sigils:mighty_leap", label: "Mighty Leap" },
-    { value: "v1644619390:Inscryption:Sigils:rabbithole", label: "Rabbithole" },
-    { value: "v1644619390:Inscryption:Sigils:random", label: "Random" },
-    { value: "v1644619390:Inscryption:Sigils:sharp_quills", label: "Sharp Quills" },
-    { value: "v1644619390:Inscryption:Sigils:sprinter", label: "Sprinter" },
-    { value: "v1644619390:Inscryption:Sigils:stinky", label: "Stinky" },
-    { value: "v1644619390:Inscryption:Sigils:touch_of_death", label: "Touch of Death" },
-    { value: "v1644613749:Inscryption:Sigils:trifurcated_strike", label: "Trifurcated Strike" },
-    { value: "v1644619390:Inscryption:Sigils:trinket_bearer", label: "Trinket Bearer" },
-    { value: "v1644619390:Inscryption:Sigils:unkillable", label: "Unkillable" },
-    { value: "v1644619391:Inscryption:Sigils:waterborne", label: "Waterborne" },
-    { value: "v1644619391:Inscryption:Sigils:worthy_sacrifice", label: "Worthy Sacrifice" },
+    ...SIGILS.map((s) => {
+      return {
+        value: `${s.id}:Inscryption:Sigils:${s.filename}`,
+        label: s.label,
+      };
+    }),
   ];
 
   useEffect(() => {
