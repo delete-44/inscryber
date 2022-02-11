a5f6c09e8586a989de6852719203f4bb1806d5f1
delete44 <delete44+tech@protonmail.com>
2022-02-11 19:22:23 +0000

    Added basic select example

--- components/sigils.js
+++ components/sigils.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Select from "react-select";
 
 const Sigils = (props) => {
+  const options = [
+    { value: "sigils/airborne", label: "Airborne" },
+    { value: "sigils/bifurcated_strike", label: "Bifurcated Strike" },
+  ];
+
   return (
     <section className="mb-10">
       <p className="mb-3">
         Does this creature have any <label htmlFor="sigils">sigils</label>?
       </p>
 
-      {/* THIS IS NOT FINAL, but a good indicator of size */}
-      <input
-        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-orange-100 bg-clip-padding border border-solid border-gray-300 rounded"
-        type="file"
-        id="portrait"
-      />
+      < Select options={options} onChange={} />
     </section>
   );
 };
