6219ea2721b117af6bcecf0ffa56b3efdfdc89c7
delete44 <delete44+tech@protonmail.com>
2022-02-14 17:08:11 +0000

    Made a change to trigger build

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  collectCoverage: true,
+  collectCoverage: false,
   moduleNameMapper: {
     "^@form_fields(.*)$": "components/form_fields/$1",
   },
