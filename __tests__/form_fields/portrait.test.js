aeedf60cc8b76049ba18f2d614d8284588113fd2
delete44 <delete44+tech@protonmail.com>
2022-02-14 20:43:37 +0000

    Added tests for successfully uploading file

--- __tests__/form_fields/portrait.test.js
+++ __tests__/form_fields/portrait.test.js
@@ -1,9 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Portrait from "@form_fields/portrait";
+import userEvent from "@testing-library/user-event";
 
 describe("Portrait", () => {
   const mockCallback = jest.fn();
+  const testFile = new File(["Test"], "test.png", { type: "image/png" });
 
   beforeEach(() => {
     render(<Portrait setPortraitTF={mockCallback} />);
@@ -30,5 +32,54 @@ describe("Portrait", () => {
 
     expect(fileFlavour).toBeInTheDocument();
     expect(fileLabel).toBeInTheDocument();
+  });
+
+  describe("when response is successful", () => {
+    beforeEach(() => {
+      fetch.mockResponseOnce(
+        JSON.stringify({
+          public_id: "fake/image/returned",
+        })
+      );
+    });
+
+    it("successfully manages state as image uploaded", async () => {
+      const fileField = screen.getByLabelText("portrait");
+      const error = screen.getByRole("alert");
+      const spinner = screen.getByRole("status");
+
+      expect(fileField).not.toBeDisabled();
+      expect(error).toHaveClass("hidden");
+      expect(spinner).toHaveClass("hidden");
+
+      userEvent.upload(fileField, testFile);
+
+      expect(fileField).toBeDisabled();
+      expect(error).toHaveClass("hidden");
+      expect(spinner).not.toHaveClass("hidden");
+
+      await waitFor(() => {
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(mockCallback).toHaveBeenCalledTimes(1);
+
+        expect(fileField).not.toBeDisabled();
+        expect(error).toHaveClass("hidden");
+        expect(spinner).toHaveClass("hidden");
+      });
+    });
+
+    it("correctly replaces invalid chars from response", async () => {
+      const fileField = screen.getByLabelText("portrait");
+
+      userEvent.upload(fileField, testFile);
+
+      await waitFor(() => {
+        expect(fetch).toHaveBeenCalledTimes(1);
+
+        expect(mockCallback).toHaveBeenCalledWith(
+          "l_fake:image:returned.webp,y_-80/"
+        );
+      });
+    });
   });
 });
