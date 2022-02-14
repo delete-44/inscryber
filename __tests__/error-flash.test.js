1873683da587e492b9ab996fb73ef942b44c49d7
delete44 <delete44+tech@protonmail.com>
2022-02-14 19:59:24 +0000

    Added tests for error message state

--- __tests__/error-flash.test.js
+++ __tests__/error-flash.test.js
@@ -14,4 +14,27 @@ describe("ErrorFlash", () => {
     expect(error).toBeInTheDocument();
     expect(message).toBeInTheDocument();
   });
+
+  it("renders custom error message when provided", () => {
+    render(<ErrorFlash hidden={false} message="Test error" />);
+
+    const alert = screen.getByRole("alert");
+    const error = screen.getByText("Error:");
+    const message = screen.getByText("Test error");
+    const defaultMessage = screen.queryByText(
+      "Something went wrong! Please try again."
+    );
+
+    expect(alert).toBeInTheDocument();
+    expect(error).toBeInTheDocument();
+    expect(message).toBeInTheDocument();
+    expect(defaultMessage).toBeNull();
+  });
+
+  it("hides alert when state demands", () => {
+    render(<ErrorFlash hidden={true} />);
+
+    const alert = screen.getByRole("alert");
+    expect(alert).toHaveClass("hidden");
+  });
 });
