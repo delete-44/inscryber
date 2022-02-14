e57ac07f15af009f9271439e2d671e8eebe94b35
delete44 <delete44+tech@protonmail.com>
2022-02-14 19:20:46 +0000

    Added busy state to component

--- components/form_fields/portrait.js
+++ components/form_fields/portrait.js
@@ -1,55 +1,71 @@
 import React, { useState, useEffect } from "react";
 import { CLOUDINARY_API_BASE } from "components/constants";
+import Spinner from "components/spinner";
 
 const Portrait = (props) => {
   const [image, setImage] = useState("");
   const { setPortraitTF } = props;
 
+  // State management for form component
+  const [busy, setBusy] = useState(false);
+
   // Queries the cloudinary API to upload an image. If successful,
   // will return an object containing a url key that we can use.
   const upload = async (imgData) => {
-    try {
-      const formData = new FormData();
-      formData.append("file", imgData);
-      formData.append(
-        "upload_preset",
-        process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
-      );
+    setBusy(true);
 
-      const response = await fetch(CLOUDINARY_API_BASE, {
-        method: "post",
-        body: formData,
-      });
+    const formData = new FormData();
+    formData.append("file", imgData);
+    formData.append(
+      "upload_preset",
+      process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
+    );
 
-      return response.json();
-    } catch (e) {
-      // FIXME: Add proper error handling
-      console.log(e);
-    }
+    const response = await fetch(CLOUDINARY_API_BASE, {
+      method: "post",
+      body: formData,
+    });
+
+    setBusy(false);
+    return response.json();
   };
 
   useEffect(async () => {
-    if (image === "") return;
+    if (image === "") {
+      setPortraitTF("");
+      return;
+    }
 
-    const uploaded = await upload(image);
+    try {
+      const uploaded = await upload(image);
 
-    // Public ID is returned with "/" characters. As we are only using
-    // the image for transformations, we replace them with ":"
-    // Sizing & fitting for the image is done on upload, handled by
-    // cloudinary, to save storage. Only need to position image in this TF.
-    setPortraitTF(`l_${uploaded.public_id.replace(/\//g, ":")}.webp,y_-80/`);
+      // Public ID is returned with "/" characters. As we are only using
+      // the image for transformations, we replace them with ":"
+      // Sizing & fitting for the image is done on upload, handled by
+      // cloudinary, to save storage. Only need to position image in this TF.
+      setPortraitTF(`l_${uploaded.public_id.replace(/\//g, ":")}.webp,y_-80/`);
+    } catch (e) {
+      // FIXME: Add proper error handling
+      console.log(e);
+      setBusy(false);
+    }
   }, [image]);
 
   return (
     <section>
-      <p className="mb-3">
-        Finally... a <label htmlFor="portrait">portrait</label>.
-      </p>
+      <span className="inline-flex justify-between w-full">
+        <p className="mb-3">
+          Finally... a <label htmlFor="portrait">portrait</label>.
+        </p>
+
+        <Spinner hidden={!busy} />
+      </span>
 
       <input
         className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-orange-100 bg-clip-padding border border-solid border-gray-300 rounded"
         type="file"
         id="portrait"
+        disabled={busy}
         onChange={(e) => {
           setImage(e.target.files[0]);
         }}
