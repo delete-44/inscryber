2048b2e7e7932e089db6efa288b7e247cfeb107c
delete44 <delete44+tech@protonmail.com>
2022-02-18 13:59:55 +0000

    Added base global SEO

---
+++ next-seo.config.js
@@ -0,0 +1,41 @@
+export default {
+  defaultTitle: "Inscryber",
+  description:
+    "Inscryber is a fan-made project celebrating Daniel Mullins' Inscryption." +
+    "Generate custom cards quickly and, hopefully, painlessly." +
+    "If you're looking for an opportunity to become the newest Scrybe, this is it.",
+  canonical: "https://www.inscryber.delete44.com",
+  twitter: {
+    cardType: "summary",
+    handle: "@_delete44",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://www.inscryber.delete44.com",
+    title: "Inscryber",
+    description:
+      "Inscryber is a fan-made project celebrating Daniel Mullins' Inscryption." +
+      "Generate custom cards quickly and, hopefully, painlessly." +
+      "If you're looking for an opportunity to become the newest Scrybe, this is it.",
+    images: [
+      {
+        url: `https://res.cloudinary.com/delete-44/image/upload/c_scale,w_600/v1644341775/Inscryption/blank_vladde.png`,
+        width: 600,
+        height: 600,
+        alt: "A blank Inscryption card",
+      },
+    ],
+  },
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: `https://res.cloudinary.com/delete-44/image/upload/c_scale,w_16/v1644341775/Inscryption/blank_vladde.png`,
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
+    },
+  ],
+};
