d087c4959107f9422ffffbb84a465f8aadae7c0c
delete44 <delete44+tech@protonmail.com>
2022-02-18 11:22:08 +0000

    Removed unneeded utility class

--- layouts/grid-layout.js
+++ layouts/grid-layout.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "components/footer";
 import Head from "next/head";
+import Footer from "components/footer";
 
 const GridLayout = (props) => {
   return (
@@ -14,7 +14,9 @@ const GridLayout = (props) => {
       <main className="min-h-screen">
         <h1 className="mt-5 mb-10">{props.title}</h1>
 
-        <div className="layout-grid">{props.children}</div>
+        <div className="md:grid md:grid-cols-2 md:gap-4 w-11/12 md:w-5/6 mx-auto">
+          {props.children}
+        </div>
       </main>
 
       <Footer />
