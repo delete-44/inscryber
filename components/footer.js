b65765404bb7c87eb458c3a32eef2ff9e791ee6c
delete44 <delete44+tech@protonmail.com>
2022-02-18 11:48:43 +0000

    Styling footer for large pages

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,20 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-orange-100 w-full inline-flex mt-12">
-      <ul className="my-3 px-4 border-r-2 border-orange-400">
+    <footer className="bg-neutral-900 w-full inline-flex mt-12 py-3">
+      <nav className="px-4 border-r-2 border-orange-400 flex flex-col text-right">
+        <Link href="/">
+          <a className="font-title text-orange-400 text-shadow-orange text-4xl">
+            Inscryber
+          </a>
+        </Link>
+
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+
+      <ul className="px-4 text-orange-100">
         <li>
           &copy; <a href="https://github.com/vladdeSV">vladdeSV</a>
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[card base
@@ -24,10 +36,6 @@ const Footer = () => {
           website]
         </li>
       </ul>
-
-      <Link href="/about">
-        <a>About</a>
-      </Link>
     </footer>
   );
 };
