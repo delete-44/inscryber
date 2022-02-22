a34f96c9aecc56580f21b4b51c4759b98aa46172
delete44 <delete44+tech@protonmail.com>
2022-02-22 17:53:56 +0000

    Migrated tests to new layout

--- __tests__/files/uploader.test.js
+++ __tests__/files/uploader.test.js
@@ -1,21 +1,20 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Portrait from "@form-fields/portrait";
+import Uploader from "components/files/uploader";
 import userEvent from "@testing-library/user-event";
 
-describe("Portrait", () => {
+describe("Uploader", () => {
   const mockCallback = jest.fn();
   const testFile = new File(["Test"], "test.png", { type: "image/png" });
 
   beforeEach(() => {
-    render(<Portrait setPortraitTF={mockCallback} />);
+    render(<Uploader setImageId={mockCallback} />);
     fetch.resetMocks();
 
     jest.clearAllMocks();
   });
 
-  it("renders uploader", () => {
-    // Form elements
+  it("renders a field upload field and hides status indicators by default", () => {
     const fileField = screen.getByLabelText("portrait");
     const error = screen.getByRole("alert");
     const spinner = screen.getByRole("status");
@@ -25,28 +24,14 @@ describe("Portrait", () => {
 
     expect(error).toHaveClass("hidden");
     expect(spinner).toHaveClass("hidden");
+  });
 
-    // Flavour text
+  it("renders a flavourful description", () => {
     const fileFlavour = screen.getByText("Finally... a .");
     const fileLabel = screen.getByText("portrait");
 
     expect(fileFlavour).toBeInTheDocument();
     expect(fileLabel).toBeInTheDocument();
-  });
-
-  it("renders privacy warning & help text", () => {
-    const privacyText = screen.getByText(
-      /Please review how we handle images in the privacy section of our before uploading./
-    );
-    const aboutLink = screen.getByRole("link", { name: "about page" });
-    const helpText = screen.getByText(
-      /Images are scaled to fit dimensions 624x512. For best results, use an image at least this size, with a transparent background./
-    );
-
-    expect(privacyText).toBeInTheDocument();
-    expect(aboutLink).toBeInTheDocument();
-    expect(aboutLink).toHaveAttribute("href", "/about");
-    expect(helpText).toBeInTheDocument();
   });
 
   describe("when response is successful", () => {
@@ -91,9 +76,38 @@ describe("Portrait", () => {
       await waitFor(() => {
         expect(fetch).toHaveBeenCalledTimes(1);
 
-        expect(mockCallback).toHaveBeenCalledWith(
-          "l_fake:image:returned/fl_layer_apply,y_-80/"
-        );
+        expect(mockCallback).toHaveBeenCalledWith("fake:image:returned");
+      });
+    });
+  });
+
+  describe("when response is error", () => {
+    beforeEach(() => {
+      fetch.mockRejectOnce("Test error");
+    });
+
+    it("successfully manages state as image upload fails", async () => {
+      const fileField = screen.getByLabelText("portrait");
+      const error = screen.getByRole("alert");
+      const spinner = screen.getByRole("status");
+
+      expect(fileField).not.toBeDisabled();
+      expect(error).toHaveClass("hidden");
+      expect(spinner).toHaveClass("hidden");
+
+      userEvent.upload(fileField, testFile);
+
+      expect(fileField).toBeDisabled();
+      expect(error).toHaveClass("hidden");
+      expect(spinner).not.toHaveClass("hidden");
+
+      await waitFor(() => {
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(mockCallback).toHaveBeenCalledTimes(0);
+
+        expect(fileField).not.toBeDisabled();
+        expect(error).not.toHaveClass("hidden");
+        expect(spinner).toHaveClass("hidden");
       });
     });
   });
