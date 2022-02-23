bf10851f93b3641ab532668080aad5c40e8561bd
delete44 <delete44+tech@protonmail.com>
2022-02-23 17:08:13 +0000

    Added pixelate effect to overlay

--- components/form-fields/portrait.js
+++ components/form-fields/portrait.js
@@ -17,7 +17,7 @@ const Portrait = (props) => {
 
     setPortraitTF(
       `l_${imageId}/` +
-        `${inscrybed ? "e_oil_paint:80/e_saturation:-80/" : ""}` +
+        `${inscrybed ? "e_pixelate:5/e_oil_paint:80/e_saturation:-90/" : ""}` +
         "fl_layer_apply,y_-80/"
     );
   }, [imageId, inscrybed]);
