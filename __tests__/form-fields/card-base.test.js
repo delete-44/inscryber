4ce09552cfad50e5d458cf51daf0795e3d09d5e4
delete44 <delete44+tech@protonmail.com>
2022-02-25 18:08:41 +0000

    Added tests for rendering field

--- __tests__/form-fields/card-base.test.js
+++ __tests__/form-fields/card-base.test.js
@@ -7,8 +7,26 @@ describe("CardBase", () => {
   const mockCallback = jest.fn();
 
   beforeEach(() => {
-    render(<CardBase setSigilsTF={mockCallback} />);
+    render(<CardBase value={"vladde"} setValue={mockCallback} />);
     jest.clearAllMocks();
+  });
+
+  it("renders radio buttons correctly", () => {
+    const normalRadio = screen.getAllByRole("radio")[0];
+    const rareRadio = screen.getAllByRole("radio")[1];
+    const unsacrificableRadio = screen.getAllByRole("radio")[2];
+
+    expect(normalRadio).toBeInTheDocument();
+    expect(normalRadio).toHaveAttribute("id", "vladde");
+    expect(normalRadio).toBeChecked();
+
+    expect(rareRadio).toBeInTheDocument();
+    expect(rareRadio).toHaveAttribute("id", "rare");
+    expect(rareRadio).not.toBeChecked();
+
+    expect(unsacrificableRadio).toBeInTheDocument();
+    expect(unsacrificableRadio).toHaveAttribute("id", "unsacrificable");
+    expect(unsacrificableRadio).not.toBeChecked();
   });
 
   it("renders a flavourful description", () => {
