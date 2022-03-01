3ffd8860d73abaca137902fa29e59bf588a9b108
delete44 <delete44+tech@protonmail.com>
2022-03-01 21:19:24 +0000

    Added better framework for tribe tests

--- __tests__/form-fields/tribes.test.js
+++ __tests__/form-fields/tribes.test.js
@@ -1,7 +1,6 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Tribes from "@form-fields/tribes";
-import selectEvent from "react-select-event";
 import userEvent from "@testing-library/user-event";
 
 describe("Tribes", () => {
@@ -35,4 +34,31 @@ describe("Tribes", () => {
     expect(tribesFlavour).toBeInTheDocument();
     expect(tribesLabel).toBeInTheDocument();
   });
+
+  it("correctly sets a transformation", async () => {
+    const birdField = screen.getByRole("checkbox", { name: /Bird/ });
+
+    userEvent.click(birdField);
+
+    expect(mockCallback).toHaveBeenLastCalledWith(
+      "l_Inscryption:ResizedTribes:bird/o_30/fl_layer_apply,g_north_west,y_32/"
+    );
+  });
+
+  it("correctly unsets specific transformations", async () => {
+    const birdField = screen.getByRole("checkbox", { name: /Bird/ });
+    const canineField = screen.getByRole("checkbox", { name: /Canine/ });
+
+    userEvent.click(birdField);
+
+    expect(mockCallback).toHaveBeenLastCalledWith(
+      "l_Inscryption:ResizedTribes:bird/o_30/fl_layer_apply,g_north_west,y_32/"
+    );
+  });
+
+  it("sorts selected tribes alphabetically regardless of selection order", async () => {});
+
+  it("correctly sets all transformations when all selected", async () => {});
+
+  it("removes the transformation when all transformations deselected", async () => {});
 });
