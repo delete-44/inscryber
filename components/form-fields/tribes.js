c581253279eb468aa6761b1d72d132a5dd88aa70
delete44 <delete44+tech@protonmail.com>
2022-03-01 16:13:11 +0000

    First look at managing state for tribes; switches from uncontrolled to controlled

--- components/form-fields/tribes.js
+++ components/form-fields/tribes.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TRIBES } from "components/constants";
 
 const Tribes = (props) => {
+  const [selectedTribes, setSelectedTribes] = useState({});
   const { setTribesTF } = props;
+
+  useEffect(() => {
+    console.log(selectedTribes);
+  }, [selectedTribes]);
 
   return (
     <section className="mb-10">
@@ -10,17 +15,20 @@ const Tribes = (props) => {
         Does this creature belong to any <label htmlFor="tribes">tribes</label>?
       </p>
 
-      <span className="flex flex-wrap w-full md:w-4/6 justify-between">
+      <span className="flex flex-wrap w-full md:w-4/6 ">
         {TRIBES.map(({ filename, label }) => {
           return (
-            <div key={filename} className="w-auto md:w-2/6">
+            <div key={filename} className="w-2/6">
               <input
                 type="checkbox"
                 name="tribes"
                 id={filename}
-                checked={props.value === filename}
-                onChange={() => {
-                  // setValue(filename);
+                checked={selectedTribes[filename]}
+                onChange={(e) => {
+                  setSelectedTribes({
+                    ...selectedTribes,
+                    [filename]: e.target.checked,
+                  });
                 }}
               />
               <label htmlFor={filename} className="text-xl md:text-2xl">
