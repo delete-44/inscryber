d42fefe3d6af071221a007a015f03b84f117884a
delete44 <delete44+tech@protonmail.com>
2022-03-02 16:05:38 +0000

    Added test for staggering requests

--- __tests__/form.test.js
+++ __tests__/form.test.js
@@ -1,15 +1,17 @@
-import { act, render, screen, waitFor } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import * as constants from "components/constants";
 import Form from "components/form";
-import selectEvent from "react-select-event";
 
 describe("Form", () => {
+  const mockSetBusy = jest.fn();
+  const mockSetUrl = jest.fn();
+
   constants.CLOUDINARY_BASE = "https://test/";
 
   beforeEach(async () => {
-    render(<Form />);
+    render(<Form setBusy={mockSetBusy} setUrl={mockSetUrl} />);
 
     jest.useFakeTimers();
     jest.clearAllMocks();
@@ -57,5 +59,49 @@ describe("Form", () => {
     expect(birdTribe).toBeInTheDocument();
     expect(rareCardBack).toBeInTheDocument();
     expect(portrait).toBeInTheDocument();
+  });
+
+  it("staggers changes across multiple fields", async () => {
+    const nameField = screen.getByRole("textbox", {
+      name: /Name/,
+    });
+
+    const powerField = screen.getByRole("spinbutton", {
+      name: /Power/,
+    });
+
+    expect(mockSetUrl).not.toHaveBeenCalled();
+    expect(mockSetBusy).not.toHaveBeenCalled();
+
+    await act(async () => {
+      userEvent.type(nameField, "123456789");
+    });
+
+    jest.advanceTimersByTime(constants.DEBOUNCE_TIMER - 1);
+
+    expect(mockSetUrl).not.toHaveBeenCalled();
+    expect(mockSetBusy).not.toHaveBeenCalled();
+
+    // Make a change before timer runs out
+    await act(async () => {
+      userEvent.type(powerField, "123456789");
+    });
+
+    expect(mockSetUrl).not.toHaveBeenCalled();
+    expect(mockSetBusy).not.toHaveBeenCalled();
+
+    // Allow timer to finish
+    await act(async () => {
+      jest.advanceTimersByTime(constants.DEBOUNCE_TIMER + 2);
+    });
+
+    // Confirm image contains both transformations
+    expect(mockSetUrl).toHaveBeenLastCalledWith(
+      "https://test/" +
+        "l_text:Inscryption:HEAVYWEIGHT.ttf_128:9,w_560,h_115,c_fit/fl_layer_apply,y_48,g_north/" +
+        "l_text:Inscryption:HEAVYWEIGHT.ttf_196:123456789,c_scale,w_100/fl_layer_apply,g_south_west,x_72,y_156/" +
+        "Inscryption/blank_vladde"
+    );
+    expect(mockSetBusy).toBeCalledWith(true);
   });
 });
