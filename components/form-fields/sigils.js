484a70c377afb54073bfc0fc162e32fdb64f6379
delete44 <delete44+tech@protonmail.com>
2022-03-03 09:22:33 +0000

    Updated sigils useEffect to support multiple sigils

--- components/form-fields/sigils.js
+++ components/form-fields/sigils.js
@@ -18,10 +18,37 @@ const Sigils = (props) => {
   ];
 
   useEffect(() => {
-    sigil === null || sigil.value === ""
-      ? setSigilsTF("")
-      : setSigilsTF(`l_${sigil.value}/fl_layer_apply,g_south,y_64/`);
-  }, [sigil, setSigilsTF]);
+    if (sigils.length === 0) {
+      setSigilsTF("");
+      return;
+    }
+
+    // Single sigils are larger and central than multiple sigils
+    if (sigils.length === 1) {
+      setSigilsTF(`l_${sigils[0].value}/fl_layer_apply,g_south,y_64/`);
+      return;
+    }
+
+    // Build array of selected values,
+    // ie ["Inscryption:ResizedSigils:airborne", "Inscryption:ResizedSigils:stinky"]
+    const sigilValues = sigils.map((p) => p.value);
+    let transformation = "";
+
+    const commonTransformations = "w_180,c_scale/fl_layer_apply,g_south,";
+    // Declare transformations for each patch
+    const sigilTransformations = [
+      `${commonTransformations}y_39,x_-75/`,
+      `${commonTransformations}y_150,x_75/`,
+    ];
+
+    // Create full TF from individual sigil transformations declared
+    // in sigilTransformations. Each line pertains to a patch
+    sigilValues.forEach((p, i) => {
+      transformation += `l_${p}/${sigilTransformations[i]}`;
+    });
+
+    setSigilsTF(transformation);
+  }, [sigils, setSigilsTF]);
 
   return (
     <section className="mb-10">
