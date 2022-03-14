b024aee91da3df3aa87984147cb0fbce12f47e5a
delete44 <delete44+tech@protonmail.com>
2022-03-14 19:30:14 +0000

    Started adding state management to cost

--- components/form-fields/dynamic-cost.js
+++ components/form-fields/dynamic-cost.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DynamicCost = (props) => {
+  const [cost, setCost] = useState("");
+  const [currency, setCurrency] = useState("blood");
+  const { setCostTF } = props;
+
   return (
     <section className="mb-10">
       <p className="mb-3">
@@ -9,7 +13,20 @@ const DynamicCost = (props) => {
       </p>
 
       <section className="grid grid-cols-2 gap-10 md:w-3/6 w-full">
-        <input type="number" aria-label="Cost" name="cost" className="my-2" />
+        <input
+          type="number"
+          aria-label="Cost"
+          name="cost"
+          className="my-2"
+          value={cost}
+          onChange={(e) => {
+            // Accepted values are from 0 (free) to 99, as we support
+            // max. 2 chars of cost.
+            if (e.target.value >= 0 && e.target.value < 100) {
+              setCost(e.target.value);
+            }
+          }}
+        />
 
         <div>
           <div className="form-check">
