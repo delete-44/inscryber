ee92525130ff45f653b180259eaf39d32ed72e45
delete44 <delete44+tech@protonmail.com>
2022-03-19 22:24:00 +0000

    Updated tests to reflect change

--- __tests__/multi-select.test.js
+++ __tests__/multi-select.test.js
@@ -11,6 +11,7 @@ describe("MultiSelect", () => {
   constants.SIGILS = [{ value: "leshy", label: "Beasts" }];
   constants.GRIMORA_SIGILS = [{ value: "grimora", label: "Dead" }];
   constants.MAGNIFICUS_SIGILS = [{ value: "magnificus", label: "Magicks" }];
+  constants.PO3_SIGILS = [{ value: "PO3", label: "Technology" }];
   constants.KAYCEE_SIGILS = [{ value: "kaycee", label: "Old Data" }];
 
   beforeEach(() => {
@@ -92,11 +93,13 @@ describe("MultiSelect", () => {
     expect(screen.getByText(/Leshy/)).toBeInTheDocument();
     expect(screen.getByText(/Grimora/)).toBeInTheDocument();
     expect(screen.getByText(/Magnificus/)).toBeInTheDocument();
+    expect(screen.getByText(/PO3/)).toBeInTheDocument();
     expect(screen.getByText(/Kaycee/)).toBeInTheDocument();
 
     expect(screen.getByText(/Beasts/)).toBeInTheDocument();
     expect(screen.getByText(/Dead/)).toBeInTheDocument();
     expect(screen.getByText(/Magicks/)).toBeInTheDocument();
+    expect(screen.getByText(/Technology/)).toBeInTheDocument();
     expect(screen.getByText(/Old Data/)).toBeInTheDocument();
   });
 });
