3d24f88bea6851a13f3823b39e9b1b6d462025d5
delete44 <delete44+tech@protonmail.com>
2022-03-20 14:40:58 +0000

    Added comment to inscryber

--- components/files/inscryber.js
+++ components/files/inscryber.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { PORTRAIT_MANIPULATIONS } from "components/constants";
 import MultiCheckbox from "components/multi-checkbox";
+import { PORTRAIT_MANIPULATIONS } from "components/constants";
 
 const Inscryber = (props) => {
   const [selectedManipulations, setSelectedManipulations] = useState([]);
   const { setInscrybedTFs } = props;
 
+  // selectedManipulations comes in as an array of transformations
+  // If any of these are present, chain them with "/" characters
+  // If not, remove the transformation completely
   useEffect(() => {
     setInscrybedTFs(
       selectedManipulations.length > 0
