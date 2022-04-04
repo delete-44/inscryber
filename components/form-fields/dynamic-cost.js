b80b11828eb9bd689e1802d7c0b0c1a759e21089
delete44 <delete44+tech@protonmail.com>
2022-04-04 20:11:33 +0100

    Destructured object

--- components/form-fields/dynamic-cost.js
+++ components/form-fields/dynamic-cost.js
@@ -8,11 +8,13 @@ const DynamicCost = (props) => {
   const { setCostTF } = props;
 
   useEffect(() => {
+    const { max, filename } = currency;
+
     // If switching to a currency with a lower max value,
     // ie blood to energy, it is possible to input a value
     // greater than the max. This eliminates that risk.
-    if (cost > currency.max) {
-      setCost(currency.max);
+    if (cost > max) {
+      setCost(max);
     }
 
     // Remove transformation if card is free
@@ -23,18 +25,16 @@ const DynamicCost = (props) => {
 
     // For as long as we have valid assets (up to 10), use them
     if (cost <= 10) {
-      setCostTF(`l_Inscryber:Costs:v2:${currency.filename}_${cost}/t_cost/`);
+      setCostTF(`l_Inscryber:Costs:v2:${filename}_${cost}/t_cost/`);
       return;
     }
 
     // For larger costs, generate them dynamically by rendering
     // a wide background, then each character of the number in line
     setCostTF(
-      `t_v2_${currency.filename}-bg-wide/` +
-        `l_Inscryber:Costs:v2:${currency.filename}:${String(cost[0])}` +
-        "/t_v2_cost-ten/" +
-        `l_Inscryber:Costs:v2:${currency.filename}:${String(cost[1])}` +
-        "/t_v2_cost-unit/"
+      `t_v2_${filename}-bg-wide/` +
+        `l_Inscryber:Costs:v2:${filename}:${String(cost[0])}/t_v2_cost-ten/` +
+        `l_Inscryber:Costs:v2:${filename}:${String(cost[1])}/t_v2_cost-unit/`
     );
   }, [cost, currency, setCostTF]);
 
