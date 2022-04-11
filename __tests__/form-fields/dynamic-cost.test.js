4cb1a53de14ec2216697eab9dd55afeb39ee055b
delete44 <delete44+tech@protonmail.com>
2022-04-11 19:40:45 +0100

    Updated tests to reflect changes

--- __tests__/form-fields/dynamic-cost.test.js
+++ __tests__/form-fields/dynamic-cost.test.js
@@ -50,19 +50,19 @@ describe("DynamicCost", () => {
     userEvent.type(costField, "1");
 
     expect(mockCallback).toHaveBeenCalledTimes(1);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-1_1/t_cost/"
-    );
+    expect(mockCallback).toHaveBeenLastCalledWith({
+      cost: { currency: "test-1", value: "1" },
+    });
 
     userEvent.type(costField, "{selectall}{backspace}");
 
     expect(mockCallback).toHaveBeenCalledTimes(2);
-    expect(mockCallback).toHaveBeenLastCalledWith("");
+    expect(mockCallback).toHaveBeenLastCalledWith({});
 
     userEvent.type(costField, "0");
 
     expect(mockCallback).toHaveBeenCalledTimes(3);
-    expect(mockCallback).toHaveBeenLastCalledWith("");
+    expect(mockCallback).toHaveBeenLastCalledWith({});
   });
 
   describe("invalid characters", () => {
@@ -104,9 +104,9 @@ describe("DynamicCost", () => {
       userEvent.type(costField, "10");
 
       expect(mockCallback).toHaveBeenCalledTimes(2);
-      expect(mockCallback).toHaveBeenLastCalledWith(
-        "l_Inscryber:Costs:v2:test-1_10/t_cost/"
-      );
+      expect(mockCallback).toHaveBeenLastCalledWith({
+        cost: { currency: "test-1", value: "10" },
+      });
 
       // Add third character
       userEvent.type(costField, "1");
@@ -121,22 +121,18 @@ describe("DynamicCost", () => {
     userEvent.type(costField, "100");
     expect(costField).toHaveValue(100);
 
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "t_v2_test-1-bg-wide/" +
-        "l_Inscryber:Costs:v2:test-1:1/t_v2_cost-ten" +
-        "/l_Inscryber:Costs:v2:test-1:0/t_v2_cost-unit/"
-    );
+    expect(mockCallback).toHaveBeenLastCalledWith({
+      cost: { currency: "test-1", value: "100" },
+    });
 
     const test2Field = screen.getByRole("radio", { name: /TEST 2/ });
     const test3Field = screen.getByRole("radio", { name: /TEST 3/ });
 
     userEvent.click(test2Field);
 
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "t_v2_test-2-bg-wide/" +
-        "l_Inscryber:Costs:v2:test-2:3/t_v2_cost-ten/" +
-        "l_Inscryber:Costs:v2:test-2:0/t_v2_cost-unit/"
-    );
+    expect(mockCallback).toHaveBeenLastCalledWith({
+      cost: { currency: "test-2", value: 30 },
+    });
 
     expect(costField).toHaveValue(30);
 
@@ -144,9 +140,9 @@ describe("DynamicCost", () => {
 
     expect(costField).toHaveValue(5);
 
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-3_5/t_cost/"
-    );
+    expect(mockCallback).toHaveBeenLastCalledWith({
+      cost: { currency: "test-3", value: 5 },
+    });
   });
 
   it("switches between currencies when radio button selection changed", () => {
@@ -155,9 +151,9 @@ describe("DynamicCost", () => {
     userEvent.type(costField, "1");
 
     expect(mockCallback).toHaveBeenCalledTimes(1);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-1_1/t_cost/"
-    );
+    expect(mockCallback).toHaveBeenLastCalledWith({
+      cost: { currency: "test-1", value: "1" },
+    });
 
     const test1Field = screen.getByRole("radio", { name: /TEST 1/ });
     const test2Field = screen.getByRole("radio", { name: /TEST 2/ });
@@ -174,9 +170,9 @@ describe("DynamicCost", () => {
     expect(test3Field).not.toBeChecked();
 
     expect(mockCallback).toHaveBeenCalledTimes(2);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-2_1/t_cost/"
-    );
+    expect(mockCallback).toHaveBeenLastCalledWith({
+      cost: { currency: "test-2", value: "1" },
+    });
 
     userEvent.click(test3Field);
 
@@ -185,67 +181,8 @@ describe("DynamicCost", () => {
     expect(test3Field).toBeChecked();
 
     expect(mockCallback).toHaveBeenCalledTimes(3);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-3_1/t_cost/"
-    );
-  });
-
-  it("uses existing assets for costs up to 10", () => {
-    const costField = screen.getByRole("spinbutton", { name: /Cost/ });
-
-    userEvent.type(costField, "1");
-
-    expect(mockCallback).toHaveBeenCalledTimes(1);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-1_1/t_cost/"
-    );
-
-    userEvent.type(costField, "0");
-
-    expect(mockCallback).toHaveBeenCalledTimes(2);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-1_10/t_cost/"
-    );
-  });
-
-  it("dynamically generates 2-character costs", () => {
-    const costField = screen.getByRole("spinbutton", { name: /Cost/ });
-
-    userEvent.type(costField, "1");
-
-    expect(mockCallback).toHaveBeenCalledTimes(1);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-1_1/t_cost/"
-    );
-
-    userEvent.type(costField, "1");
-
-    expect(mockCallback).toHaveBeenCalledTimes(2);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "t_v2_test-1-bg-wide/" +
-        "l_Inscryber:Costs:v2:test-1:1/t_v2_cost-ten/" +
-        "l_Inscryber:Costs:v2:test-1:1/t_v2_cost-unit/"
-    );
-
-    userEvent.type(costField, "{selectall}{backspace}");
-
-    expect(mockCallback).toHaveBeenCalledTimes(3);
-    expect(mockCallback).toHaveBeenLastCalledWith("");
-
-    userEvent.type(costField, "9");
-
-    expect(mockCallback).toHaveBeenCalledTimes(4);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "l_Inscryber:Costs:v2:test-1_9/t_cost/"
-    );
-
-    userEvent.type(costField, "9");
-
-    expect(mockCallback).toHaveBeenCalledTimes(5);
-    expect(mockCallback).toHaveBeenLastCalledWith(
-      "t_v2_test-1-bg-wide/" +
-        "l_Inscryber:Costs:v2:test-1:9/t_v2_cost-ten/" +
-        "l_Inscryber:Costs:v2:test-1:9/t_v2_cost-unit/"
-    );
+    expect(mockCallback).toHaveBeenLastCalledWith({
+      cost: { currency: "test-3", value: "1" },
+    });
   });
 });
