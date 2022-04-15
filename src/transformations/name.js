b421cf1dc08631bc5ec64043b4f840db2e509fc9
delete44 <delete44+tech@protonmail.com>
2022-04-15 10:51:37 +0100

    Removed unused params in constructors

--- src/transformations/name.js
+++ src/transformations/name.js
@@ -2,7 +2,7 @@ import { Transformation } from "../transformation";
 import { HEAVYWEIGHT } from "components/constants";
 
 export class NameTransformation extends Transformation {
-  constructor(value, _type) {
+  constructor(value) {
     super(value, "name");
   }
 
