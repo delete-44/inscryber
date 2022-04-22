67fab6c4b361da4326a3880b1d1bb0eee8ddf3ea
delete44 <delete44+tech@protonmail.com>
2022-04-22 12:53:40 +0100

    Added tests for act 3 sigils TF

--- __tests__/src/transformations/sigils.test.js
+++ __tests__/src/transformations/sigils.test.js
@@ -8,21 +8,46 @@ describe("SigilsTransformation", () => {
       expect(tf.toString()).toEqual("");
     });
 
-    it("generates correct transformation string for single sigil", () => {
-      const tf = new SigilsTransformation(["test-sigil-1"]);
+    describe("when a single sigil", () => {
+      it("generates correct transformation string", () => {
+        const tf = new SigilsTransformation(["test-sigil-1"]);
 
-      expect(tf.toString()).toEqual(
-        "l_Inscryber:Sigils:v1:test-sigil-1/t_sigil/"
-      );
+        expect(tf.toString()).toEqual(
+          "l_Inscryber:Sigils:v1:test-sigil-1/t_sigil/"
+        );
+      });
+
+      it("adds act three modifier if needed", () => {
+        const tf = new SigilsTransformation(["test-sigil-1"], {
+          isActThree: true,
+        });
+
+        expect(tf.toString()).toEqual(
+          "l_Inscryber:Sigils:v1:test-sigil-1/t_act_3_sigil/"
+        );
+      });
     });
 
-    it("generates correct transformation string for multiple sigils", () => {
-      const tf = new SigilsTransformation(["test-sigil-1", "test-sigil-2"]);
+    describe("when multiple sigils", () => {
+      it("generates correct transformation string", () => {
+        const tf = new SigilsTransformation(["test-sigil-1", "test-sigil-2"]);
 
-      expect(tf.toString()).toEqual(
-        "l_Inscryber:Sigils:v1:test-sigil-1/t_v1_sigil_1/" +
-          "l_Inscryber:Sigils:v1:test-sigil-2/t_v1_sigil_2/"
-      );
+        expect(tf.toString()).toEqual(
+          "l_Inscryber:Sigils:v1:test-sigil-1/t_v1_sigil_1/" +
+            "l_Inscryber:Sigils:v1:test-sigil-2/t_v1_sigil_2/"
+        );
+      });
+
+      it("adds act three modifier if needed", () => {
+        const tf = new SigilsTransformation(["test-sigil-1", "test-sigil-2"], {
+          isActThree: true,
+        });
+
+        expect(tf.toString()).toEqual(
+          "l_Inscryber:Sigils:v1:test-sigil-1/t_v1_act_3_sigil_1/" +
+            "l_Inscryber:Sigils:v1:test-sigil-2/t_v1_act_3_sigil_2/"
+        );
+      });
     });
   });
 });
