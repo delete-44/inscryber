f25252c4f4a44b675b10e62a744aae36f7ca6ce2
delete44 <delete44+tech@protonmail.com>
2022-04-24 15:05:11 +0100

    Additional colours added to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,15 @@ module.exports = {
       colors: {
         orange: {
           100: "#fff1e7",
+          200: "#ffb583",
+          300: "#ead7d1",
           400: "#cc6f33",
         },
         blue: {
           100: "#f7fffe",
-          400: "#7DCBDA",
+          200: "#bbfff7",
+          300: "#cffff9",
+          400: "#7dcbda",
         },
         red: "#b72e38",
         gray: "#424242",
