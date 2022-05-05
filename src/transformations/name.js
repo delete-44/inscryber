a31ddb4ba26da6fdc729f853dff1334b2118f065
delete44 <delete44+tech@protonmail.com>
2022-05-05 16:52:07 +0100

    Cited source

--- src/transformations/name.js
+++ src/transformations/name.js
@@ -15,6 +15,9 @@ export class NameTransformation extends Transformation {
     // Prefer different transformation for short strings
     const tfLength = this.value.length < 12 ? "short" : "long";
 
+    // Entire overlay is double-encoded as per this article:
+    // https://support.cloudinary.com/hc/en-us/articles/202521512
+    // "Generally it would be a good practice to double-escape the entire overlaid text"
     return (
       `l_text:${HEAVYWEIGHT}_128_center:` +
       `${encodeURIComponent(encodeURIComponent(this.value))}/` +
