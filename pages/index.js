73e4210b7363442b969cecc5abdf6274cf36eacf
delete44 <delete44+tech@protonmail.com>
2022-05-11 19:31:29 +0100

    Removed image from index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React, { useState } from "react";
 import {
   CARD_BASE,
@@ -7,9 +6,8 @@ import {
   CARD_HEIGHT,
 } from "components/constants";
 import Form from "components/form";
-import Spinner from "components/spinner";
 import GridLayout from "layouts/grid-layout";
-import Link from "next/link";
+import ImagePreview from "components/image-preview";
 
 export default function Home() {
   // State management for this component
@@ -34,32 +32,7 @@ export default function Home() {
       />
 
       {/* Right column */}
-      <div className="width-full flex flex-col justify-center relative mt-16 md:mt-0 h-min sticky top-8">
-        <Spinner hidden={!busy} />
-
-        <Image
-          src={url}
-          alt="A preview of your custom card"
-          width={busy ? 0 : CARD_WIDTH * 0.6}
-          height={busy ? 0 : CARD_HEIGHT * 0.6}
-          objectFit="contain"
-          objectPosition="center top"
-          priority
-          onLoadingComplete={() => {
-            setBusy(false);
-          }}
-        />
-
-        <Link href={url}>
-          <a
-            target="_blank"
-            className={`mx-auto mt-8 mb-2 text-center ${busy ? "hidden" : ""}`}
-            tabIndex={-1}
-          >
-            <button>Download Image</button>
-          </a>
-        </Link>
-      </div>
+      <ImagePreview setBusy={setBusy} busy={busy} url={url} />
     </GridLayout>
   );
 }
