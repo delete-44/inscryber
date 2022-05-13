66f2a940435a5c03dab222604fa310f4c46a5868
delete44 <delete44+tech@protonmail.com>
2022-05-13 15:47:02 +0100

    Updated tests to reflect new syntax

--- __tests__/src/utils/url-helper.test.js
+++ __tests__/src/utils/url-helper.test.js
@@ -33,7 +33,7 @@ describe("generateUrl", () => {
         "l_text:Inscryber:HEAVYWEIGHT.ttf_128_center:TEST%2520CARD/t_name_short/" +
         "l_text:Inscryber:HEAVYWEIGHT.ttf_196:0,c_scale,w_65/t_power/" +
         "l_text:Inscryber:HEAVYWEIGHT.ttf_196:120,c_scale,w_100/t_health/" +
-        "l_Inscryber:Sigils:v1:test_sigil_1/t_sigil/" +
+        "l_Inscryber:Sigils:v1:test_sigil_1/t_sigil_1_of_1/" +
         "l_Inscryber:overlays:v1:test_overlay_1/" +
         "l_Inscryber:patches:v1:test_patch_1/t_patch_1/" +
         `${CARD_BASE}test_card_base`
